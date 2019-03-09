fb5a8621465f76c55981de2e8a8bdfc5542df40d
Igu <igoralissoncn@gmail.com>
2019-03-09 19:53:32 -0300

    Update ex7.c

--- ListaFun3/ex7.c
+++ ListaFun3/ex7.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
 int main(void) {
-  int i, v1 = 1, v2, soma = 0;
-  for(v2 = 1; v2 <= 50; v2++, v1++) soma += (v1++)/v2;
-  printf("%d\n",soma);
+  int i, v1 = 1, v2; 
+  float soma = 0;
+  for(v2 = 1; v2 <= 50; v2++, v1++) soma += ((v1++)+0.00)/v2;
+  printf("%.2f\n",soma);
   return 0;
 }
