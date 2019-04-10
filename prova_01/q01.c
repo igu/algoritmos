a8d1ce760e87595d4ba660a17454df62abbea79d
Igu <igoralissoncn@gmail.com>
2019-04-10 12:26:02 -0300

    Update q01.c

--- prova_01/q01.c
+++ prova_01/q01.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
 int main(void) {
-  int n, i, soma = 0;
+  int n, i;
+  float soma = 0;
   scanf("%d",&n);
-  for(i = 1; i<n; i++)
-    soma += ((1/i)-(1/++i));
+  for(i = 1; i < n; i += 2)
+    soma += ((1/(i+0.0))-(1.0/(i+1.0)));
+  printf("Soma: %f\n",soma);
   return 0;
 }
