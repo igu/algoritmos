4c0988fc9e5dbd166ec822d57b999f2352ef5646
Igu <igoralissoncn@gmail.com>
2019-05-28 13:53:13 -0300

    Update example.c

--- timeCount/example.c
+++ timeCount/example.c
@@ -8,14 +8,17 @@ int main(void) {
    inicio_t = clock(); // GET TIME INICIAL
 
    printf("Inicio = %ld\n", inicio_t);
-    
+
    printf("Iniciando o grande loop: inicio_t =  %ld\n", inicio_t);
-   
-   for(i = 0; i < 100000; i++) printf(".");
+
+   for(i = 0; i < 10000000; i++){
+        if(!(i%10000)) printf("%d\n",i);
+   }
+
 
    fim_t = clock(); // GET TIME FINAL
    printf("\nFim do grande loop, fim_t = %ld\n", fim_t);
-   
+
    total_t = (double)((fim_t - inicio_t) / (CLOCKS_PER_SEC / 1000));
    printf("Sua CPU rodou: %ld\n", total_t);
   return 0;
